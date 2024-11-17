acbd2502df659d06e5579b0f73c0e005f39d5ee4
tony-alignnutrition <tony@align-nutrition.com>
2024-11-17 16:59:27 -0700

    Add toDateInputValue util

---
+++ src/utils/toDateInputValue.ts
@@ -0,0 +1,5 @@
+export default function toDateInputValue(date: Date) {
+  const local = new Date(date);
+  local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  return local.toJSON().slice(0, 10);
+}
