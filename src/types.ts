26c9a57e617389b6348768e89447c2a5eb83c9e9
tony-alignnutrition <tony@align-nutrition.com>
2024-11-17 16:27:58 -0700

    Update Employee interface

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,10 @@
-export interface Employee {}
+export interface Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  department: string;
+  dateStarted: string;
+  quote: string;
+  status: string;
+  avatarUrl: string;
+}
